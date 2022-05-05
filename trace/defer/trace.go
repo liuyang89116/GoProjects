d5757f73eb1d1014754df8dd189a2c471ec7790e
liuyang89116 <liuyang89116@qq.com>
2022-05-05 00:37:55 -0700

    Add trace demo

---
+++ trace/defer/trace.go
@@ -0,0 +1,31 @@
+package main
+
+func Trace(name string) func() {
+	println("enter:", name)
+	return func() {
+		println("exit:", name)
+	}
+}
+
+func foo() {
+	defer Trace("foo")()
+	bar()
+}
+
+func bar() {
+	defer Trace("bar")()
+}
+
+func main() {
+	defer Trace("main")()
+	foo()
+}
+
+/*
+	enter: main
+	enter: foo
+	enter: bar
+	exit: bar
+	exit: foo
+	exit: main
+*/
